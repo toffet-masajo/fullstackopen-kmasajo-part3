13213bff0fa5028bf1b7fe65ef6b22274f3b13bc
toffet-masajo <kcmasajo@yahoo.com>
2023-03-17 02:37:20 +0800

    3.7 Phonebook backend step7

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const MAX_ENTRIES = 100;
 
 const app = express();
 app.use(express.json());
+app.use(morgan('tiny'));
 
 let persons = [
   { 
