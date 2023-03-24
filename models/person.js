d400fda0450e6297299b4e16f6b06e7a5174547b
toffet-masajo <kcmasajo@yahoo.com>
2023-03-25 02:47:28 +0800

    3.20* Phonebook database step8

--- models/person.js
+++ models/person.js
@@ -17,9 +17,18 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
-    required: true
+    required: [true, 'Name is required']
   },
-  number: String
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{6,}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'Phone number is required']
+  }
 });
 
 const Person = mongoose.model('Person', personSchema);
