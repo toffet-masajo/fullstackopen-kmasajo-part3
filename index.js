da8d691edfc714f459ada3d64c6e62f8ce2deeeb
toffet-masajo <kcmasajo@yahoo.com>
2023-03-29 00:36:02 +0800

    Fixed linting errors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static('build'));
 
-morgan.token('type', function(req, res) {return JSON.stringify(req.body);});
+morgan.token('type', function(req, res) {res; return JSON.stringify(req.body);});
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'));
 
 app.use(cors());
